4f3db3b04a11186ea5aaa7b1c088735081172ea3
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-03-20 16:07:40 +0900

    [add]HPバーの追加、走るアニメーションの追加

---
+++ Assets/Sasaki/Scripts/PlayerHp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHp : MonoBehaviour
+{
+
+    [SerializeField] public int maxHp;      //最大体力
+    [SerializeField] public int hp;         //体力
+    int cnt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //現在の体力を最大体力に設定する
+        hp = maxHp;
+        cnt = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (hp <= 0)
+        {
+            Debug.Log("死亡しました");
+        }
+
+        if(cnt == 60)
+        {
+            hp -= 1;
+            cnt = 0;
+        }
+
+
+        cnt++;
+    }
+}
