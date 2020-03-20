261e2e932feb1c07aeb4052a9c870f74ef0ec026
澤村遼 <ryo882514@gmail.com>
2020-03-20 17:24:46 +0900

    [update]左クリックで球発射に変更 右クリックしながらマウスを振るとチャージするように

--- Assets/S/Shooting.cs
+++ Assets/S/Shooting.cs
@@ -14,46 +14,37 @@ public class Shooting : MonoBehaviour
     // 弾丸の速度
     public float speed = 1000;
 
-    // 弾丸の速度
-    public float power = 0;
+    public float charge= 0.5f;
 
-    private int Charge = 0;
     private bool flg = false;
+    private bool chackflg = false;
+    private float cnt = 0;
 
-    float x = 0;
-    float y = 0;
-    float z = 0;
+    private Vector3 chack;
+    private Vector3 mousePosition;
 
 
     // Use this for initialization
     void Start()
     {
-        Vector3 chack = new Vector3(x, y, z);
+        bullet.transform.localScale = new Vector3(charge, charge, charge);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        //  右クリックが押されている状態なら
+        if (Input.GetMouseButtonDown(1))
         {
-            bullet.transform.localScale += new Vector3(0.5f,0.5f,0.5f);
-                /* Vector3 mousePosition = Input.mousePosition;//マウスを振れば弾が大きくなるようにしたい
-                  flg = true;
-                  if (flg == true)
-                  {
-                Vector3 chack = Input.mousePosition;//マウスを振れば弾が大きくなるようにしたい
-                if (mousePosition.x == chack.x)
-                      {
-
-                      }
-                  }*/
+            flg = true;
         }
-        if (Input.GetKeyDown(KeyCode.A))//大きさを元に戻す(テスト用)
+        //  右クリックが離されている状態なら
+        if (Input.GetMouseButtonUp(1))
         {
-            bullet.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            flg = false;
         }
-        // z キーが押された時
-        if (Input.GetKeyDown(KeyCode.Z))
+        // 左クリックが押された時
+        if (Input.GetMouseButtonDown(0))
         {
                 // 弾丸の複製
                 GameObject bullets = Instantiate(bullet) as GameObject;
@@ -68,7 +59,65 @@ public class Shooting : MonoBehaviour
                 // 弾丸の位置を調整
                 bullets.transform.position = muzzle.position;
 
-                Destroy(bullets, 3.0f); // 三秒後に削除
-         }
+       　　     // 三秒後に削除
+        　　    Destroy(bullets, 3.0f);
+
+                // 弾の大きさを元に戻す
+                charge = 0.5f;
+                bullet.transform.localScale = new Vector3(charge, charge, charge);
+        }
+        // 右クリックが押され続けているなら
+        if (flg == true)
+        {
+            // カウント
+            if (cnt == 0)
+            {
+                // マウスの座標を取得(Aとする)
+                mousePosition = Input.mousePosition;
+            }
+            // カウントアップ
+            cnt++;
+
+            if (cnt == 10)
+            {
+                // マウスの座標を取得(Bとする)
+                chack = Input.mousePosition;
+
+                // マウスAとマウスBの座標を比べる一定以上動いていればマウスを振っていると認識
+                // 振っているのを確認するため、chackflgがtrueだと右に振ったfalseだと左に振った
+                // マウスAとマウスBは一定の間隔で座標を取得するため、cntでカウントされている
+
+                //右に振った
+                if (mousePosition.x > chack.x + 50 && chackflg == true) 
+                {
+                    // 弾チャージ
+                    bullet.transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+                    charge += 0.05f;
+
+                    // フラグを反転
+                    chackflg = false;
+                }
+
+                //左に振った
+                if (mousePosition.x < chack.x - 50 && chackflg == false)
+                {
+                    // 弾チャージ
+                    bullet.transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+                    charge += 0.05f;
+
+                    // フラグを反転
+                    chackflg = true;
+                }
+                // カウントリセット
+                cnt = 0;
+            }
+        }
+
+        // チャージ上限
+        if(charge >= 2)
+        {
+            charge = 2;
+            bullet.transform.localScale = new Vector3(charge, charge, charge);
+        }
     }
 }
