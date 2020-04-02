3ef6f2ae283ea9a934b9bf6989bd3f380385eb56
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-04-02 18:16:08 +0900

    [add]ゲーム進行に必要なシーンの追加＆シーン変移を仮置き

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //ステージ１にシーンを変更する
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Game_St01");
+        }
+
+        //ステージ２にシーンを変更する
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Game_St02");
+        }
+
+        //ステージ３にシーンを変更する
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene("Game_St03");
+        }
+    }
+}
