0715ac5b92e3efcf71419d45cd897ab167f70b51
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-04-20 17:16:46 +0900

    [fix]スライムの向きを修正

--- Assets/Urabe/Script/Enemy/enemSlimemove.cs
+++ Assets/Urabe/Script/Enemy/enemSlimemove.cs
@@ -15,18 +15,18 @@ public class enemSlimemove : MonoBehaviour
     public float targetdistance;       //目標位置までの距離
 
     //ランダムの目標位置を決める
-    Vector3 GetRandomPosition(Vector3 currentpos)
+    /*Vector3 GetRandomPosition(Vector3 currentpos)
     {
         return new Vector3(Random.Range(-posrange + currentpos.x, posrange + currentpos.x), 0, Random.Range(-posrange + currentpos.z, posrange + currentpos.z));
-    }
+    }*/
 
     //ただ徘徊するだけ
     void haikai()
     {
-        if (targetdistance < changetarget) targetpos = GetRandomPosition(transform.position);
+        //if (targetdistance < changetarget) targetpos = GetRandomPosition(transform.position);
 
         Quaternion targetRotation = Quaternion.LookRotation(targetpos - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationspeed);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationspeed);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
         //Debug.Log("haikai");
     }
@@ -37,7 +37,7 @@ public class enemSlimemove : MonoBehaviour
 
     private void Start()
     {
-        targetpos = GetRandomPosition(transform.position);
+        //targetpos = GetRandomPosition(transform.position);
         _agent = GetComponent<NavMeshAgent>();
     }
 
