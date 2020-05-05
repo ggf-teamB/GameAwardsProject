17c1dd9e77b06356211d9a94d68d0932a7d592a7
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-05-05 17:11:47 +0900

    [add]ドアの付近に当たり判定を加える

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("今日のごはんは浦部とリューロウの和え物");
+        }
+    }
+}
