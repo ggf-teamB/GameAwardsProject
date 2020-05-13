96f680c21ab0bec858781323e982e04492f39b30
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-05-13 16:50:05 +0900

    [add]アイテムドロップ

--- Assets/Urabe/Script/Item/MobItemDropper.cs
+++ Assets/Urabe/Script/Item/MobItemDropper.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using Random = UnityEngine.Random;
 
-[RequireComponent(typeof(MobStatus))]
+[RequireComponent(typeof(CountDownHP.zako_status))]
 public class MobItemDropper : MonoBehaviour
 {
     [SerializeField] [Range(0, 1)] private float dropRate = 0.1f;
@@ -9,15 +9,15 @@ public class MobItemDropper : MonoBehaviour
     [SerializeField] private Item itemPrefab;
     [SerializeField] private int number = 1; 　//アイテム出現個数
 
-    private MobStatus _status;
+    public CountDownHP.zako_status _status;
     private bool _isDropInvoked;
     private void Start()
     {
-        _status = GetComponent<MobStatus>();
+        _status = GetComponent<CountDownHP.zako_status>();
     }
     private void Update()
     {
-        if (_status.Life <= 0)
+        if (_status.HP <= 0)
         {
             //ライフが尽きたときに実行
             DropIfNeeded();
