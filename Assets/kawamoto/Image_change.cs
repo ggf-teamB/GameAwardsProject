cf6fbc4a6745fac7fe9c255c780c57867c813186
Flamberg3983 <masaki1217jp@gmail.com>
2020-05-14 16:23:23 +0900

    [add]アイコンとかの追加

---
+++ Assets/kawamoto/Image_change.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+using UnityEngine.UI;
+
+public class Image_change : MonoBehaviour
+{
+    Sprite sprite;
+    Image  Dush_image;
+    Image Nomal_image;
+
+    void Start()
+    {
+        sprite = Resources.Load<Sprite>("Unity02");//ダッシュ画像を読み込み
+        Nomal_image = GetComponent<Image>();
+    }
+
+    
+    void Update()
+    {    //SpriteRenderのspriteを設定済みの他のspriteに変更
+
+        Debug.Log("aaaaaaaa");
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) ||
+            Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) )
+        {
+            sprite = Resources.Load<Sprite>("Unity02");//ダッシュ画像を読み込み
+            Dush_image = GetComponent<Image>();
+            Dush_image.sprite = sprite; //ダッシュ画像に変更
+        }
+        else
+        {
+            sprite = Resources.Load<Sprite>("Unity01");//通常画像を読み込み
+            Nomal_image = GetComponent<Image>();
+            Nomal_image.sprite = sprite; //通常画像に変更
+        }
+  
+    }
+}
---
+++ Assets/kawamoto/UI_NAMEcamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_NAMEcamera : MonoBehaviour
+{
+
+    public GameObject Monster_name;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Loteupdate();
+    }
+
+    // Update is called once per frame
+    void Loteupdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        Monster_name = transform.Find("MS_NAME").gameObject;
+        Debug.Log("urabename");
+    }
+}
