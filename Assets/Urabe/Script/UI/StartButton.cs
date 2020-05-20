747241de39da77cc5fb8eba02526da877accc2da
bontenmarukamisama <bontenmarukamisama@gmail.com>
2020-05-20 18:03:04 +0900

    [add]タイトル画面の作成

---
+++ Assets/Urabe/Script/UI/StartButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        //ボタンを押したとき
+        button.onClick.AddListener(() =>
+        {
+            //シーン偏移の際にはSceneManagerを使用
+            SceneManager.LoadScene("Menu");
+        });
+    }
+}
