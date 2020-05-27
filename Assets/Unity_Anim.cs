053acae28484b8839708557d7b17d08ad2c974a5
Flamberg3983 <masaki1217jp@gmail.com>
2020-05-27 13:27:01 +0900

    [up]Unityちゃんのアニメーションを少しでも良くなるように。限界。

---
+++ Assets/Unity_Anim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unity_Anim : MonoBehaviour
+{
+
+    Animator animator;
+
+    //Inspectorで変更できるようにpublicにした
+    public float first;
+    public int second;
+    public float third;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //マウス左クリックで発動
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("ururururur");
+            animator.CrossFade("Gun", first, second, third);
+        }
+    }
+}
