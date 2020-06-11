8f1cb20feb7448541c24d6507fbba1f923c3e54d
kuroneko96 <kaityougakusyuuyou@gmail.com>
2020-06-12 01:43:09 +0900

    [add]ゲームクリアシーンを追加

---
+++ Assets/Scripts/Scene/GameClear.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameClear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スペースキーを入力
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
